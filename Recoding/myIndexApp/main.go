93fb72d141df886a787b54399a548139af55704b
maxchichar <maxwellchibuezecharles@gmail.com>
2026-03-25 16:27:01 +0100

    recoding index completed

---
+++ Recoding/myIndexApp/main.go
@@ -0,0 +1,80 @@
+//last word
+//capitalise
+//delete ask the user the index to delete and delete it. if the user put five delete the word at index 5, if index is 10 and the user input 25 it should tell the user that the index is out of range try again by restarting the program
+
+//last word
+//capitalise
+//delete ask the user the index to delete and delete it. if the user put five delete the word at index 5, if index is 10 and the user input 25 it should tell the user that the index is out of range try again by restarting the program
+
+package main
+
+import (
+	"fmt"
+	"time"
+	"strings"
+)
+
+var result string
+var word string
+
+func indexApp(word string) string {	
+	fmt.Println("Welcome To My Index App")
+	time.Sleep(3 * time.Second)
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("Input a Word: ")
+	fmt.Scanln(&word)
+	fmt.Println()
+
+	fmt.Println("Select An Operation")
+	fmt.Println()
+
+	operation1 := "Lastword"
+	operation2 := "Capitalise"
+	operation3 := "Delete-index"
+
+	fmt.Println("1. ", operation1)
+	fmt.Println("2. ", operation2)
+	fmt.Println("3. ", operation3)
+	fmt.Println()
+	fmt.Scanln(&result)
+
+	switch result {
+	case "Lastword":
+		return string(word[len(word)-1])
+	case "Capitalise":
+		return strings.ToUpper(word)
+	case "Delete-index":
+		fmt.Println()
+		fmt.Println("Input index you want to delete: ")
+		var deleteUserIndex int
+		fmt.Scanln(&deleteUserIndex)
+		fmt.Println()
+
+		lWord := len(word)
+
+		for {
+			if deleteUserIndex >= lWord || deleteUserIndex < 0 {
+				fmt.Printf("Index out of range!, input the an index within the range of %v: ", lWord)
+				fmt.Scanln(&word)
+				continue
+			}
+			break
+		}
+
+		emptyRune := []rune{}
+
+		for i, ch := range word {
+			if deleteUserIndex != i {
+				emptyRune = append(emptyRune, ch)
+			}
+		}
+
+		return string(emptyRune)
+	}
+	return "Invalid Operation"
+}
+
+func main() {
+	fmt.Println(indexApp(result))
+}
---
+++ Recoding/myIndexApp/question.md
@@ -0,0 +1,9 @@
+Write a function that ask a user to input a word then ask a user to select an operation by either entering lastword or capitalize or delete index.
+
+if `lastword` inputted by user, the program should return the last character on the word inputted.
+
+if `capitalize` inputted by the user, the program should return the word capitalized
+
+if `deleteIndex` inputted by the user, the program should as the user to input index, when index inputted, the character at that index should be deleted and the rest of the letters in the word be returned.
+
+else if the index inputted is greater than the length of the word, the program should display an error message and restart that delete index block of code
